0ed699e0ccd4d347356eef01a54aed040bbe774f
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2026-02-12 14:42:04 +0000

    Added template switching

--- src/FinderServiceProvider.php
+++ src/FinderServiceProvider.php
@@ -8,17 +8,26 @@ class FinderServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $this->mergeConfigFrom(
+            __DIR__ . '/config.php',
+            'laravel-find',
+        );
     }
 
     public function boot(): void
     {
+        $template = config('laravel-find.template') ?? 'default';
+
         $this->publishes([
-            __DIR__ . '/components' => resource_path('views/vendor/laravel-find'),
-        ], 'laravel-find');
+            __DIR__ . 'config.php' => config_path('laravel-find.php'),
+        ], 'laravel-find-config');
+
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/laravel-find'),
+        ], 'laravel-find-views');
 
         $this->loadViewsFrom(
-            __DIR__,
+            __DIR__ . "/views/$template",
             'laravel-find',
         );
     }
