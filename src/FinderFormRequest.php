c1e85d6c923cc816657c2d5fcab8a3ab95795b85
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2026-02-16 12:06:08 +0000

    Added tests

---
+++ src/FinderFormRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AnthonyEdmonds\LaravelFind;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class FinderFormRequest extends FormRequest
+{
+    public Finder $finder;
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            $this->finder::KEY_FILTER => [
+                'nullable',
+                'string',
+                Rule::in(
+                    array_keys(
+                        $this->finder->listFilters(),
+                    ),
+                ),
+            ],
+            $this->finder::KEY_SEARCH => [
+                'nullable',
+                'string',
+                'min:1',
+            ],
+            $this->finder::KEY_SORT => [
+                'nullable',
+                'string',
+                Rule::in(
+                    array_keys(
+                        $this->finder->listSorts(),
+                    ),
+                ),
+            ],
+            $this->finder::KEY_STATUS => [
+                'nullable',
+                'string',
+                Rule::in(
+                    array_keys(
+                        $this->finder->listStatuses(),
+                    ),
+                ),
+            ],
+        ];
+    }
+}
---
+++ tests/Unit/Finder/ValidateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AnthonyEdmonds\LaravelFind\Tests\Unit\Finder;
+
+use AnthonyEdmonds\LaravelFind\Finder;
+use AnthonyEdmonds\LaravelFind\Tests\TestCase;
+use AnthonyEdmonds\LaravelFind\Tests\TestFinder;
+use Illuminate\Validation\ValidationException;
+use Throwable;
+
+class ValidateTest extends TestCase
+{
+    protected Finder $finder;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+
+    }
+
+    public function testPasses(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        request()->query->set(TestFinder::KEY_FILTER, 'author');
+        request()->query->set(TestFinder::KEY_SEARCH, '213');
+        request()->query->set(TestFinder::KEY_SORT, 'newest');
+        request()->query->set(TestFinder::KEY_STATUS, 'all');
+
+        try {
+            $this->finder = new TestFinder();
+        } catch (Throwable $exception) {
+            $this->fail("An exception was thrown when it should not have been: {$exception->getMessage()}");
+        }
+    }
+
+    public function testFails(): void
+    {
+        $this->expectException(ValidationException::class);
+        $this->expectExceptionMessage('The selected filter is invalid. (and 3 more errors)');
+
+        request()->query->set(TestFinder::KEY_FILTER, 'aosdk');
+        request()->query->set(TestFinder::KEY_SEARCH, 213);
+        request()->query->set(TestFinder::KEY_SORT, 'aosdk');
+        request()->query->set(TestFinder::KEY_STATUS, 'aosdk');
+
+        $this->finder = new TestFinder();
+    }
+}
