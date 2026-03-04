3f3cd9f5b6e6bf1bd87a457cd08b32a2dc5e729b
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-03-04 12:37:20 +0000

    Fixed form request for empty filters

--- src/FinderFormRequest.php
+++ src/FinderFormRequest.php
@@ -17,39 +17,50 @@ class FinderFormRequest extends FormRequest
 
     public function rules(): array
     {
-        return [
-            $this->finder::KEY_FILTER => [
-                'nullable',
-                'string',
-                Rule::in(
-                    array_keys(
-                        $this->finder->listFilters(),
-                    ),
-                ),
-            ],
-            $this->finder::KEY_SEARCH => [
-                'nullable',
-                'string',
-                'min:1',
-            ],
-            $this->finder::KEY_SORT => [
-                'nullable',
-                'string',
-                Rule::in(
-                    array_keys(
-                        $this->finder->listSorts(),
-                    ),
-                ),
-            ],
-            $this->finder::KEY_STATUS => [
-                'nullable',
-                'string',
-                Rule::in(
-                    array_keys(
-                        $this->finder->listStatuses(),
-                    ),
-                ),
-            ],
+        $filters = array_keys(
+            $this->finder->listFilters(),
+        );
+
+        $sorts = array_keys(
+            $this->finder->listSorts(),
+        );
+
+        $statuses = array_keys(
+            $this->finder->listStatuses(),
+        );
+
+        $rules = [];
+
+        $rules[$this->finder::KEY_SEARCH] = [
+            'nullable',
+            'string',
+            'min:1',
         ];
+
+        if (empty($filters) === false) {
+            $rules[$this->finder::KEY_FILTER] = [
+                'nullable',
+                'string',
+                Rule::in($filters),
+            ];
+        }
+
+        if (empty($sorts) === false) {
+            $rules[$this->finder::KEY_SORT] = [
+                'nullable',
+                'string',
+                Rule::in($sorts),
+            ];
+        }
+
+        if (empty($statuses) === false) {
+            $rules[$this->finder::KEY_STATUS] = [
+                'nullable',
+                'string',
+                Rule::in($statuses),
+            ];
+        }
+
+        return $rules;
     }
 }
